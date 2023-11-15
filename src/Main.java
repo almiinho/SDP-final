66395efb04af34ca3edeeb1889cff338b1da8aa5
Almaz <almazazamatov863@gmail.com>
2023-11-15 17:15:33 +0600

    1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class MediaPlayerCLI {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
